fbd0d7f1a659388c38081b3129789edfeb87bc64
Robert Deans <robdeans@gmail.com>
2017-02-22 13:46:26 -0500

    pod inits with snapkit

---
+++ NeuroRunner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeuroRunner
+//
+//  Created by Robert Deans on 2/22/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
