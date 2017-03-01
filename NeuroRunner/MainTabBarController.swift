ee1dd27bf30c730efd711ad658974901afef3808
Robert Deans <robdeans@gmail.com>
2017-03-01 15:41:18 -0500

    adds tabBarController, profile view, and profile vc

---
+++ NeuroRunner/MainTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.swift
+//  NeuroRunner
+//
+//  Created by Robert Deans on 3/1/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    lazy var homeNC = UINavigationController(rootViewController: HomeViewController())
+    lazy var profile = UINavigationController(rootViewController: ProfileViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NeuroRunner/ProfileView.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileView.swift
+//  NeuroRunner
+//
+//  Created by Robert Deans on 3/1/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileView: UIView {
+    
+
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+        configure()
+        constrain()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func constrain() {
+
+    }
+}
