00c493cd76cf202e482dff86d4182df5640f92c7
Robert Deans <robdeans@gmail.com>
2017-04-21 14:56:54 -0400

    adds custom TimerView class

---
+++ NeuroRunner/TimerView.swift
@@ -0,0 +1,36 @@
+//
+//  TimerVier.swift
+//  NeuroRunner
+//
+//  Created by Robert Deans on 4/21/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimerView: UIView {
+
+    
+    
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        constrain()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func constrain() {
+        
+    }
+    
+}
