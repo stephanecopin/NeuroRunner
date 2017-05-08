4c1c6363ff84e65ff60c37bbc55a0c569565f1ef
Robert Deans <robdeans@gmail.com>
2017-05-08 15:47:06 -0400

    edits balance view

--- NeuroRunner/BalanceView.swift
+++ NeuroRunner/BalanceView.swift
@@ -15,48 +15,26 @@ class BalanceView: UIView {
     let balanceViewModel = BalanceViewModel()
     
     // Timer UI Elements
-    let customTimerView = CustomPickerView()
-    var timerLabelView = UIView()
-    
-    var secondsLabel = UILabel() {
-        didSet {
-            secondsLabel.text = "\(seconds)"
-            if 0...9 ~= seconds {
-                secondsLabel.text = "0\(seconds)"
-            }
-        }
-    }
-    var minutesLabel = UILabel() {
-        didSet {
-            minutesLabel.text = "\(minutes):"
-            if minutes == 0 {
-                minutesLabel.text = "00:"
-            }
-        }
-    }
+    let pickerView = CustomPickerView()
+    var timerView = CustomTimerLabel()
+    var segmentedControl: CustomSegmentedControl!
     
     // Timer Hardware
-    var viewTimer = Timer()
     var isTimerOn = false
-    
-    var totalTime = 0
-    var seconds: Int {
-        get {
-            return (totalTime % 60)
-        }
-    }
-    var minutes: Int {
-        get {
-            return (totalTime / 60)
-        }
-    }
+    let minLabel = UILabel()
     
     var timerDirection: Direction {
-        if upDownSegmentedControl.selectedSegmentIndex == 0 {
+        if segmentedControl.selectedIndex == 0 {
             balanceViewModel.timerDirection = .Down
+            timerView.timerDirection = .Down
+            pickerView.isHidden = false
+            print("down")
             return .Down
         } else {
             balanceViewModel.timerDirection = .Up
+            timerView.timerDirection = .Up
+            pickerView.isHidden = true
+            print("up")
             return .Up
         }
     }
@@ -64,7 +42,6 @@ class BalanceView: UIView {
     // Exercise buttons
     let startStopButton = UIButton()
     lazy var breathingButton = UIButton() // TODO manual balance?
-    var upDownSegmentedControl: UISegmentedControl!
 
     
     // MARK: Initialization
@@ -81,9 +58,9 @@ class BalanceView: UIView {
     
     func configure() {
         
-        let items = ["Down", "Up"]
-        upDownSegmentedControl = UISegmentedControl(items: items)
-        upDownSegmentedControl.selectedSegmentIndex = 1
+        segmentedControl = CustomSegmentedControl()
+        segmentedControl.items = ["Count Down", "Count Up"]
+        segmentedControl.selectedIndex = 0
         
         startStopButton.backgroundColor = UIColor.startButtonStart
         startStopButton.setTitle("Start", for: .normal)
@@ -92,52 +69,42 @@ class BalanceView: UIView {
         startStopButton.titleLabel?.font = UIFont(name: "MarkerFelt-Thin", size: 36)
         startStopButton.addTarget(self, action: #selector(startStopButtonTapped), for: .touchUpInside)
         
-        secondsLabel.font = UIFont(name: "AvenirNext-UltraLight", size: 75)
-        secondsLabel.textColor = UIColor.white
-        secondsLabel.text = "00"
-        secondsLabel.textAlignment = .center
-        secondsLabel.adjustsFontSizeToFitWidth = true
-        
-        minutesLabel.font = UIFont(name: "AvenirNext-UltraLight", size: 75)
-        minutesLabel.textColor = UIColor.white
-        minutesLabel.text = "00:"
-        minutesLabel.textAlignment = .center
-        minutesLabel.adjustsFontSizeToFitWidth = true
-        
+        minLabel.text = "  minutes"
+        minLabel.font = UIFont(name: "AvenirNext-Regular", size: 30)
+        minLabel.textColor = UIColor.white
 
     }
     
     func constrain() {
-        addSubview(timerLabelView)
-        timerLabelView.snp.makeConstraints {
+        
+        addSubview(segmentedControl)
+        segmentedControl.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(85)
+            $0.width.equalToSuperview().multipliedBy(0.75)
+            $0.height.equalToSuperview().dividedBy(15)
+        }
+        
+        addSubview(timerView)
+        timerView.snp.makeConstraints {
             $0.width.centerX.equalToSuperview()
             $0.height.equalToSuperview().dividedBy(10)
-            $0.centerY.equalToSuperview().offset(-180)
+            $0.top.equalTo(segmentedControl.snp.bottom).offset(10)
         }
         
-        timerLabelView.addSubview(minutesLabel)
-        minutesLabel.snp.makeConstraints {
-            $0.top.bottom.height.equalToSuperview()
-            $0.trailing.equalTo(timerLabelView.snp.centerX).offset(10)
+        addSubview(pickerView)
+        pickerView.snp.makeConstraints {
+            $0.width.equalToSuperview().dividedBy(8)
+            $0.height.equalToSuperview().dividedBy(9)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalTo(self.snp.centerX)
+            //            $0.centerX.equalToSuperview()
         }
         
-        timerLabelView.addSubview(secondsLabel)
-        secondsLabel.snp.makeConstraints {
-            $0.top.bottom.height.equalToSuperview()
-            $0.leading.equalTo(timerLabelView.snp.centerX).offset(10)
-        }
-        
-        addSubview(customTimerView)
-        customTimerView.snp.makeConstraints {
-            $0.width.equalToSuperview().multipliedBy(0.8)
-            $0.height.equalToSuperview().dividedBy(10)
-            $0.centerX.centerY.equalToSuperview()
-        }
-        
-        addSubview(upDownSegmentedControl)
-        upDownSegmentedControl.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(customTimerView.snp.bottom).offset(25)
+        addSubview(minLabel)
+        minLabel.snp.makeConstraints {
+            $0.centerY.equalTo(pickerView.snp.centerY)
+            $0.leading.equalTo(pickerView.snp.trailing)
         }
         
         addSubview(startStopButton)
@@ -167,51 +134,29 @@ class BalanceView: UIView {
     
     func timerOn() {
         isTimerOn = true
+        pickerView.isHidden = true
+        minLabel.isHidden = true
         
         startStopButton.setTitle("Stop", for: .normal)
         startStopButton.backgroundColor = UIColor.startButtonStop
         
-        viewTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimerLabel), userInfo: nil, repeats: true)
+        timerView.timerOn()
         
-        customTimerView.isHidden = true
+        pickerView.isHidden = true
         
     }
     
     func timerOff() {
-        viewTimer.invalidate()
+        timerView.timerOff()
         
         startStopButton.setTitle("Start", for: .normal)
         startStopButton.backgroundColor = UIColor.startButtonStart
+
         
-        secondsLabel.text = "00"
-        minutesLabel.text = "00:"
-        
-        customTimerView.isHidden = false
+        pickerView.isHidden = false
+        minLabel.isHidden = false
         isTimerOn = false
         
-    }
-    
-    func updateTimerLabel() {
-        
-        if timerDirection == .Down {
-            totalTime -= 5
-        } else {
-            totalTime += 5
-        }
-        
-        // TODO: Remaining logic should be didSet
-        secondsLabel.text = "\(seconds)"
-        minutesLabel.text = "\(minutes):"
-        if 0...9 ~= seconds {
-            secondsLabel.text = "0\(seconds)"
-        }
-        if minutes == 0 {
-            minutesLabel.text = "00:"
-        }
-        
-        if totalTime == 0 {
-            timerOff()
-        }
     }
     
 }
